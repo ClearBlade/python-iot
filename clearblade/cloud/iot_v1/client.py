b010d854d6e0620a7ef24156bfeb3b26183fc19d
Gargi <gargidas19188@gmail.com>
2022-11-02 00:07:24 +0530

    Completed the device data class and added method to create a device

--- clearblade/cloud/iot_v1/client.py
+++ clearblade/cloud/iot_v1/client.py
@@ -1,10 +1,14 @@
-from devices import SendCommandToDeviceRequest, ClearBladeDeviceManager
+from devices import SendCommandToDeviceRequest, CreateDeviceRequest, ClearBladeDeviceManager
 
 class DeviceManagerClient():
 
     def send_command_to_device(self,request:SendCommandToDeviceRequest):
         cb_device_manager = ClearBladeDeviceManager()
         return cb_device_manager.send_command(request)
+
+    def create_device(self, request):
+        cb_device_manager = ClearBladeDeviceManager()
+        return cb_device_manager.create(request=request)
 
 
 
--- clearblade/cloud/iot_v1/developer_tests.py
+++ clearblade/cloud/iot_v1/developer_tests.py
@@ -1,10 +1,18 @@
 from client import DeviceManagerClient
-from devices import SendCommandToDeviceRequest
+from devices import Device, SendCommandToDeviceRequest, CreateDeviceRequest 
 
 def test_device_manager_client():
     client = DeviceManagerClient()
-    request = SendCommandToDeviceRequest(name='Rashmi_Device_Test', binary_data=b'R2FyZ2l0ZXN0aW5n')
+    request = SendCommandToDeviceRequest(name='python_1', binary_data=b'R2FyZ2l0ZXN0aW5n')
     response = client.send_command_to_device(request)
     print(response)
 
-test_device_manager_client()
+def test_create_device():
+    client = DeviceManagerClient()
+    device = Device(id="Python_9", name="Python_9")
+
+    request = CreateDeviceRequest(device=device)
+    response = client.create_device(request=request)
+    print(response)
+
+test_create_device()
