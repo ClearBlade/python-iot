f17b78f8dc507c13a676cc6dc9b0ac354fa5b3f2
Das <gargi.das@emerson.com>
2022-11-01 09:59:55 +0530

    Added test details from rashmis key

--- clearblade/cloud/iot_v1/developer_tests.py
+++ clearblade/cloud/iot_v1/developer_tests.py
@@ -3,7 +3,7 @@ from devices import SendCommandToDeviceRequest
 
 def test_device_manager_client():
     client = DeviceManagerClient()
-    request = SendCommandToDeviceRequest(name='python_1', binary_data=b'QUJD')
+    request = SendCommandToDeviceRequest(name='Rashmi_Device_Test', binary_data=b'R2FyZ2l0ZXN0aW5n')
     response = client.send_command_to_device(request)
     print(response)
 
