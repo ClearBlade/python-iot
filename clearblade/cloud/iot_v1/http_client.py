fa411f85607e5ab26e09ccf13859744b36ac841f
Mandar Barve <mandarbarve@ingresssoftware.com>
2022-11-07 22:25:49 +0530

    Device Delete and Device Get API implementation

--- clearblade/cloud/iot_v1/http_client.py
+++ clearblade/cloud/iot_v1/http_client.py
@@ -37,8 +37,9 @@ class HttpClient():
     def _process_request_body(self, request_body = {}):
         return json.dumps(request_body)
 
-    def get(self, request_params, request_body):
-        pass
+    def get(self, request_params):
+        self._post_url = self._cb_api_url+ self._process_request_params(request_params=request_params)
+        self._request_headers = self._headers()        
 
     def post(self, request_params = {}, request_body = {}):
         self._post_url = self._cb_api_url+ self._process_request_params(request_params=request_params)
@@ -46,8 +47,9 @@ class HttpClient():
         self._post_body = self._process_request_body(request_body=request_body)
         print("post_url = {}\nheaders = {}\nbody= {}\n".format(self._post_url,self._request_headers,self._post_body))
 
-    def delete(self, request_params, request_body):
-        pass
+    def delete(self, request_params):
+        self._post_url = self._cb_api_url+ self._process_request_params(request_params=request_params)
+        self._request_headers = self._headers()
 
 class SyncClient(HttpClient):
 
@@ -58,7 +60,20 @@ class SyncClient(HttpClient):
         response = httpx_sync_client.request("POST", url=self._post_url,
                                             headers=self._request_headers, data=self._post_body)
         return response
+    
+    def delete(self, request_params):
+        super().delete(request_params=request_params)
+        httpx_sync_client = httpx.Client()
+        response = httpx_sync_client.request("DELETE", url=self._post_url,
+                                            headers=self._request_headers)
+        return response
 
+    def get(self, request_params = {}):
+        super().get(request_params=request_params)
+        httpx_sync_client = httpx.Client()
+        response = httpx_sync_client.request("GET", url=self._post_url,
+                                            headers=self._request_headers)
+        return response
 
 class AsyncClient(HttpClient):
 
@@ -67,5 +82,13 @@ class AsyncClient(HttpClient):
         httpx_async_client= httpx.AsyncClient()
         response = await httpx_async_client.request("POST", url=self._post_url,
                                                     headers=self._request_headers, data=self._post_body)
+        await httpx_async_client.aclose()
+        return response
+
+    async def delete(self, request_params):
+        super().delete(request_params=request_params)
+        httpx_async_client= httpx.AsyncClient()
+        response = await httpx_async_client.request("DELETE", url=self._post_url,
+                                            headers=self._request_headers)
         await httpx_async_client.aclose()
         return response
